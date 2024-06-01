d691bdd59e89ef9ed15d9a8f78ba9d563e32d403
elpresidank <benjamin@krieg.cloud>
2024-06-01 17:43:45 -0500

    starting on landing page

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,18 +1,16 @@
-import React from "react";
 import Footer from '@/app/components/Footer';
 import Header from '@/app/components/Header';
 import WelcomeCard from '@/app/components/WelcomeCard';
-
+import React from 'react';
 
 export default function Page() {
-    return (
-        <div className="flex flex-col min-h-screen">
-            <Header/>
-            <main className="flex-1 flex items-center justify-center">
-                <WelcomeCard/>
-            </main>
-            {' '}
-            <Footer />
-        </div>
-    );
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-1 flex items-center justify-center">
+        <WelcomeCard />
+      </main>{' '}
+      <Footer />
+    </div>
+  );
 }
