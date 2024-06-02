34758b75e3a9167577fc75320d88cdaac5184d3c
elpresidank <benjamin@krieg.cloud>
2024-06-02 17:17:50 -0500

    more tooling

---
+++ scripts/reset-versions.ts
@@ -0,0 +1,36 @@
+/* eslint-disable security/detect-non-literal-fs-filename */
+import * as fs from 'node:fs';
+import * as path from 'node:path';
+
+// Define the new version
+const newVersion = '0.1.0';
+
+/**
+ * Function to recursively find package.json files and update the version
+ * @param dir the directory to start the recursive find from.
+ */
+function updateVersion(dir: string): void {
+  const files = fs.readdirSync(dir);
+  for (const file of files) {
+    const fullPath = path.join(dir, file);
+
+    if (fs.lstatSync(fullPath).isDirectory()) {
+      // Skip node_modules directory
+      if (file !== 'node_modules') {
+        updateVersion(fullPath);
+      }
+    } else if (file === 'package.json') {
+      const packageJson = JSON.parse(fs.readFileSync(fullPath, 'utf8')) as {
+        version: string;
+      };
+      packageJson.version = newVersion;
+      fs.writeFileSync(fullPath, `${JSON.stringify(packageJson, null, 2)}\n`);
+      console.log(`Updated version in ${fullPath}`);
+    }
+  }
+}
+
+// Start updating from the current directory
+updateVersion(process.cwd());
+
+/* eslint-enable security/detect-non-literal-fs-filename */
